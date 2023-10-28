013b803583fabafa646cbcb4325c2e90fc76aa7d
Danila Khlebokazov <danilik_2003@mail.ru>
2023-10-28 15:58:58 +0600

    like button

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import "./Post.css"
 export default function Post(props){
     const {post} = props;
     const {id, postName, postInfo, viewNum, likes} = post;
+    const [likess, setLikes] = useState(likes);
+
+    const addLikes = () => {
+        setLikes(likess + 1);
+    }
+
     return <>
     <div className="post">
         <div><h3>{postName}</h3></div>
         <p> {postInfo}</p>
         views: {viewNum}
-        likes: {likes}
+        likes: {likess}
+        <button className="likebutton" onClick={addLikes}>Like</button>
     </div>
+
     </>
 }
